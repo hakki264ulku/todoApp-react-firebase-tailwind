79c6e0a83046d9795147c98d41087484ff11f631
Hakkı ÜLKÜ <54121286+hakki264ulku@users.noreply.github.com>
2020-10-19 14:06:58 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 
+// You need to enter your own firebase config values
 let firebaseConfig = {
   apiKey: "*********************",
   authDomain: "my-app-1e64f.firebaseapp.com",
